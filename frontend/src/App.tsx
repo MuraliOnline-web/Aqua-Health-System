53927148943d2ec0e180f8df50cce3e081417f26
Murali Krishna M <medakayalamuraliyadav@gmail.com>
2026-02-24 02:15:05 +0530

    Updated frontend pages, services, UI changes and config fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Home from "./pages/Home";
+import Scan from "./pages/Scan";
+import Result from "./pages/Result";
+import DiseaseLibrary from "./pages/DiseaseLibrary";
+import DiseaseDetail from "./pages/DiseaseDetail";
+import History from "./pages/History";
+import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,8 +21,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/" element={<Home />} />
+          <Route path="/scan" element={<Scan />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/disease-library" element={<DiseaseLibrary />} />
+          <Route path="/disease-library/:id" element={<DiseaseDetail />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
